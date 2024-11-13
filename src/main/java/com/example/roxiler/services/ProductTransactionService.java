51cadab3bdf5a5dc10477c2e8beac7bc0ba6cbe8
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2024-11-13 11:40:03 +0530

    feat: api update get all transactions

--- src/main/java/com/example/roxiler/services/ProductTransactionService.java
+++ src/main/java/com/example/roxiler/services/ProductTransactionService.java
@@ -5,42 +5,23 @@ import com.example.roxiler.dtos.PriceRangeDTO;
 import com.example.roxiler.dtos.StatisticsDTO;
 import com.example.roxiler.models.ProductTransaction;
 import com.example.roxiler.repositories.ProductTransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductTransactionService {
     private final ProductTransactionRepository repository;
 
-    public ProductTransactionService(ProductTransactionRepository repository) {
-        this.repository = repository;
-    }
-
     private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
-//    public List<ProductTransaction> listTransactions(int month, String search, Pageable pageable) {
-//        Page<ProductTransaction> page;
-//
-//        if (search != null && !search.isEmpty()) {
-//            page = repository.findByMonthAndSearch(month, search, pageable);
-//        } else {
-//            page = repository.findByMonth(month, pageable);
-//        }
-//
-//        return page.getContent();
-//    }
-//
-//    public boolean isTransactionInMonth(String dateString, int month) {
-//        LocalDateTime date = LocalDateTime.parse(dateString, formatter);
-//        return date.getMonthValue() == month;
-//    }
     public List<ProductTransaction> listTransactions(int month, String search, Pageable pageable) {
         Page<ProductTransaction> page;
 
@@ -52,6 +33,12 @@ public class ProductTransactionService {
 
         return page.getContent();
     }
+
+    public boolean isTransactionInMonth(String dateString, int month) {
+        LocalDateTime date = LocalDateTime.parse(dateString, formatter);
+        return date.getMonthValue() == month;
+    }
+
     public StatisticsDTO getStatistics(int month) {
         // Calculate total sale amount, sold items, and unsold items
         return null;
